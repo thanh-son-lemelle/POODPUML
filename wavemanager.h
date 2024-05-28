fb52e1c1fa6c36dbd608a66087b46dc84e0e32e6
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-05-28 14:30:15 +0200

    <refactor><cmakelists><wavemanager>:<wavemanager destructor is set public, adding objectpoolmanager files into cmakelists>

--- wavemanager.h
+++ wavemanager.h
@@ -13,6 +13,7 @@ class WaveManager : QObject
     Q_OBJECT
 public:
     static WaveManager& getInstance();
+    ~WaveManager() = default;
     void startNextWave();
     void spawnWave();
     void onLastWaveCleared();
@@ -22,13 +23,12 @@ signals:
 
 private:
     explicit WaveManager(QObject *parent = nullptr);
-    ~WaveManager() = default;
     WaveManager(const WaveManager&) = delete;
 
     static std::unique_ptr<WaveManager> instance;
     static std::once_flag initInstanceFlag;
 
-    event lastWaveClearedEvent;
+    Event lastWaveClearedEvent;
     ObjectPoolManager objectPoolManager;
 };
 
