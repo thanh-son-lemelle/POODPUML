d1ff2ccac3a8354a6a3225fae9a8f01e83035cf5
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-06-03 11:29:44 +0200

    <create>:< event class wwith listener>

--- Event.cpp
+++ Event.cpp
@@ -1,3 +1,16 @@
 #include "Event.h"
+#include <algorithm>
 
-Event::Event() {}
+void Event::addListener(const Listener& listener) {
+    listeners.push_back(listener);
+}
+
+void Event::removeListener(const Listener& listener) {
+    listeners.erase(std::remove(listeners.begin(), listeners.end(), listener), listeners.end());
+}
+
+void Event::notifyListeners() const {
+    for (const auto& listener : listeners) {
+        listener();
+    }
+}
