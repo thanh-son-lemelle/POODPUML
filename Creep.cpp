269eebf4a1a5dcb3bdcbddac3ffa59ed8956735b
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-06-07 12:33:13 +0200

    <merge>[Creep]:<added collisions with projectiles>

--- Creep.cpp
+++ Creep.cpp
@@ -1,8 +1,14 @@
 #include "Creep.h"
+#include "ObjectPool.h" 
 
 Creep::Creep(int hp, float spd, QVector2D pos) : health(hp), speed(spd), position(pos) {}
 
 void Creep::update(float deltaTime) {
+    move(deltaTime);
+    onCollisionWithProjectile();
+}
+
+void Creep::move(float deltaTime) {
     // Move logic
     position += QVector2D(1, 0) * deltaTime * speed;
 }
@@ -24,4 +30,23 @@ void Creep::draw(QPainter &painter) {
     QPixmap image(":/Creep.png");
     QPixmap scaledImage = image.scaled(WIDTH, HEIGHT, Qt::KeepAspectRatio, Qt::SmoothTransformation);
     painter.drawPixmap(position.toPointF(), scaledImage);
+}
+
+void Creep::onCollisionWithProjectile()
+{
+    ObjectPool &objectPool = ObjectPool::getInstance();
+    std::list<Projectile *> projectilesToRemove;
+    for (Projectile *projectile : objectPool.getProjectiles())
+    {
+        if (projectile->getPosition().distanceToPoint(position) < 20)
+        {
+            takeDamage(projectile->getDamage());
+            projectilesToRemove.push_back(projectile);
+        }
+    }
+
+    for (Projectile *projectile : projectilesToRemove)
+    {
+        objectPool.removeProjectile(projectile);
+    }
 }
